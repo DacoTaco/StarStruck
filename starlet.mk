3fa3f2ea3eb6720b33ecd237fd79c748b9b686a6
DacoTaco <daco_65@hotmail.com>
2021-02-28 16:14:01 +0100

    make project buildable with devkitarm

--- starlet.mk
+++ starlet.mk
@@ -1,8 +1,9 @@
-ifeq ($(strip $(WIIDEV)),)
-$(error "Set WIIDEV in your environment.")
+ifeq ($(strip $(DEVKITARM)),)
+$(error "Set DEVKITARM in your environment.")
 endif
 
-PREFIX = $(WIIDEV)/bin/armeb-eabi-
+PREFIX = $(DEVKITARM)/bin/arm-none-eabi-
+#PREFIX = $(WIIDEV)/bin/armeb-eabi-
 
 CFLAGS = -mbig-endian -mcpu=arm926ej-s
 CFLAGS += -fomit-frame-pointer -ffunction-sections
