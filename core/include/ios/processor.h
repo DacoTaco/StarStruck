1e477592dddc204b3d620932fee47b0f28035ea0
DacoTaco <daco_65@hotmail.com>
2026-04-27 21:27:03 +0200

    refactor: use volatile instead of asm

--- core/include/ios/processor.h
+++ core/include/ios/processor.h
@@ -43,141 +43,94 @@ Copyright (C) 2008, 2009	Hector Martin "marcan" <marcan@marcansoft.com>
 
 static inline u32 read32(u32 addr)
 {
-	u32 data;
-	__asm__ volatile("ldr\t%0, [%1]" : "=l"(data) : "l"(addr));
-	return data;
+	return *(vu32 *)addr;
 }
 
 static inline void write32(u32 addr, u32 data)
 {
-	__asm__ volatile("str\t%0, [%1]" : : "l"(data), "l"(addr));
+	*(vu32 *)addr = data;
 }
 
 static inline u32 set32(u32 addr, u32 set)
 {
-	u32 data;
-	__asm__ volatile("ldr\t%0, [%1]\n"
-	                 "\torr\t%0, %2\n"
-	                 "\tstr\t%0, [%1]"
-	                 : "=&l"(data)
-	                 : "l"(addr), "l"(set));
+	u32 data = read32(addr) | set;
+	write32(addr, data);
 	return data;
 }
 
 static inline u32 clear32(u32 addr, u32 clear)
 {
-	u32 data;
-	__asm__ volatile("ldr\t%0, [%1]\n"
-	                 "\tbic\t%0, %2\n"
-	                 "\tstr\t%0, [%1]"
-	                 : "=&l"(data)
-	                 : "l"(addr), "l"(clear));
+	u32 data = read32(addr) & ~clear;
+	write32(addr, data);
 	return data;
 }
 
 static inline u32 mask32(u32 addr, u32 clear, u32 set)
 {
-	u32 data;
-	__asm__ volatile("ldr\t%0, [%1]\n"
-	                 "\tbic\t%0, %3\n"
-	                 "\torr\t%0, %2\n"
-	                 "\tstr\t%0, [%1]"
-	                 : "=&l"(data)
-	                 : "l"(addr), "l"(set), "l"(clear));
+	u32 data = (read32(addr) & ~clear) | set;
+	write32(addr, data);
 	return data;
 }
 
 static inline u16 read16(u32 addr)
 {
-	u16 data;
-	__asm__ volatile("ldrh\t%0, [%1]" : "=l"(data) : "l"(addr));
-	return data;
+	return *(vu16 *)addr;
 }
 
 static inline void write16(u32 addr, u16 data)
 {
-	__asm__ volatile("strh\t%0, [%1]" : : "l"(data), "l"(addr));
+	*(vu16 *)addr = data;
 }
 
 static inline u16 set16(u32 addr, u16 set)
 {
-	u16 data;
-	__asm__ volatile("ldrh\t%0, [%1]\n"
-	                 "\torr\t%0, %2\n"
-	                 "\tstrh\t%0, [%1]"
-	                 : "=&l"(data)
-	                 : "l"(addr), "l"(set)
-
-	);
+	u16 data = read16(addr) | set;
+	write16(addr, data);
 	return data;
 }
 
 static inline u16 clear16(u32 addr, u16 clear)
 {
-	u16 data;
-	__asm__ volatile("ldrh\t%0, [%1]\n"
-	                 "\tbic\t%0, %2\n"
-	                 "\tstrh\t%0, [%1]"
-	                 : "=&l"(data)
-	                 : "l"(addr), "l"(clear));
+	u16 data = read16(addr) & ~clear;
+	write16(addr, data);
 	return data;
 }
 
 static inline u16 mask16(u32 addr, u16 clear, u16 set)
 {
-	u16 data;
-	__asm__ volatile("ldrh\t%0, [%1]\n"
-	                 "\tbic\t%0, %3\n"
-	                 "\torr\t%0, %2\n"
-	                 "\tstrh\t%0, [%1]"
-	                 : "=&l"(data)
-	                 : "l"(addr), "l"(set), "l"(clear));
+	u16 data = (read16(addr) & ~clear) | set;
+	write16(addr, data);
 	return data;
 }
 
 static inline u8 read8(u32 addr)
 {
-	u8 data;
-	__asm__ volatile("ldrb\t%0, [%1]" : "=l"(data) : "l"(addr));
-	return data;
+	return *(vu8 *)addr;
 }
 
 static inline void write8(u32 addr, u8 data)
 {
-	__asm__ volatile("strb\t%0, [%1]" : : "l"(data), "l"(addr));
+	*(vu8 *)addr = data;
 }
 
 static inline u8 set8(u32 addr, u8 set)
 {
-	u8 data;
-	__asm__ volatile("ldrb\t%0, [%1]\n"
-	                 "\torr\t%0, %2\n"
-	                 "\tstrb\t%0, [%1]"
-	                 : "=&l"(data)
-	                 : "l"(addr), "l"(set));
+	u8 data = read8(addr) | set;
+	write8(addr, data);
 	return data;
 }
 
 static inline u8 clear8(u32 addr, u8 clear)
 {
-	u8 data;
-	__asm__ volatile("ldrb\t%0, [%1]\n"
-	                 "\tbic\t%0, %2\n"
-	                 "\tstrb\t%0, [%1]"
-	                 : "=&l"(data)
-	                 : "l"(addr), "l"(clear));
+	u8 data = read8(addr) & ~clear;
+	write8(addr, data);
 	return data;
 }
 
 static inline u8 mask8(u32 addr, u8 clear, u8 set)
 {
-	u8 data;
-	__asm__ volatile("ldrb\t%0, [%1]\n"
-	                 "\tbic\t%0, %3\n"
-	                 "\torr\t%0, %2\n"
-	                 "\tstrb\t%0, [%1]"
-	                 : "=&l"(data)
-	                 : "l"(addr), "l"(set), "l"(clear));
+	u8 data = (read8(addr) & ~clear) | set;
+	write8(addr, data);
 	return data;
 }
 
